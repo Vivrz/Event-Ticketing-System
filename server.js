4690257f965394ace30389d3a2a2bf5113e482df
Vivrz <163974647+Vivrz@users.noreply.github.com>
2025-01-27 08:56:30 +0530

    update

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ connectionWithDB();
 const app = express();
 require('dotenv').config();
 app.use(cors({
-    origin: ['https://event-ticket-system-r4qa4vrbn-vivrzs-projects.vercel.app'],
+    origin: ['https://event-ticket-system-r4qa4vrbn-vivrzs-projects.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 app.use(express.json());
