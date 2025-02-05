edc6278da21129eb7455a0c2a510659ef3b89fc4
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2025-02-06 01:09:46 +0530

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
