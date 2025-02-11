20a1c1e0b74ebb11f7f997680f5fd3d53ec00b63
Vivrz <163974647+Vivrz@users.noreply.github.com>
2025-02-11 23:42:45 +0530

    puhs

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Featured from './Featured'
-import Footer from './Footer'
-import Public_events from './public_events'
+// import React from 'react'
+// import Navbar from './Navbar'
+// import Featured from './Featured'
+// import Footer from './Footer'
+// import Public_events from './public_events'
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//         <Navbar />
+//         <Featured />
+//         <br />
+//         <Public_events/>
+//         <Footer />
+//     </div>
+//   )
+// }
+
+// export default HomePage;
+import React from "react";
+import Navbar from "./Navbar";
+import Featured from "./Featured";
+import Footer from "./Footer";
+import PublicEvents from "./Public_events"; // Ensure case matches file name
 
 const HomePage = () => {
   return (
-    <div>
-        <Navbar />
-        <Featured />
-        <br />
-        <Public_events/>
-        <Footer />
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Featured />
+      <br />
+      <PublicEvents />
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
