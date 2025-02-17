0fbc475193542e8b221dc27579cf032b365c1bbd
Vivrz <163974647+Vivrz@users.noreply.github.com>
2025-02-17 10:39:29 +0530

    c

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   return (
     <>
       <div>
-        <div className={styles.navbar-container}>
+        <div className={styles.Navbar}>
           <div className={styles.logo}>
             <span className={styles.logo_text}>MVX</span>
           </div>
