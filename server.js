55f6a927560cd2b599dbcd63ee16a1f896245203
Vivrz <163974647+Vivrz@users.noreply.github.com>
2025-02-26 01:51:03 +0530

    c

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectionWithDB().catch(err => {
 const app = express();
 require('dotenv').config();
 app.use(cors({
-    origin: ['https://event-ticket-system-tan.vercel.app','http://localhost:5173/'],
+    origin: ['https://event-ticket-system-tan.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
